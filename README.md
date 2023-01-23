454d77af7d52afb94e5071b259701917aab4fc43
louisevst <louisevassart@gmail.com>
2023-01-23 11:04:08 +0100

    fixed name of countries graph 2, putted custom colours for graph 2 + refactor + build

---
+++ README.md
@@ -0,0 +1,19 @@
+# Visualisation Data
+
+As part of the BeCode training we had to make graphes.
+
+Two graphes had to be done with the html tables in the index.html. We couldn't touch the index.html.
+
+The third graph had to refresh every seconds and was done with an external page containing data points.
+
+## Tech Used
+
+- JavaScript
+- pnpm
+- vite
+
+## Develop
+
+1. `git clone` project
+2. `pnpm install` to install dependencies
+3. `pnpm dev` to start developing
--- main.js
+++ main.js
@@ -16,309 +16,24 @@ for (var i = 2; i < table1.rows.length; i++) {
   countries.push(table1.rows[i].cells[1].innerHTML);
 }
 
-//stats for belgium in the order of year
+//stats all countries
 function addAllDataCoutries(emptyArray, rowNumber) {
+  emptyArray = [];
   for (var i = 2; i < table1.rows[1].cells.length; i++) {
     let x = table1.rows[rowNumber].cells[i].innerHTML;
-    console.log(x);
     x = x.replace(",", ".");
     x = Number(x);
     emptyArray.push(x);
   }
+  data1.push(emptyArray);
 }
 let newCountry = [];
-console.log(table1.rows.length);
+let data1 = [];
 
 for (var i = 2; i < table1.rows.length; i++) {
   addAllDataCoutries(newCountry, i);
 }
 
-console.log(newCountry);
-let Belgium = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[2].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Belgium.push(x);
-}
-
-let Bulgaria = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[3].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Bulgaria.push(x);
-}
-
-let Czech = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[4].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Czech.push(x);
-}
-let Denmark = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[5].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Denmark.push(x);
-}
-let Germany = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[6].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Germany.push(x);
-}
-let Estonia = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[7].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Estonia.push(x);
-}
-let Ireland = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[8].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Ireland.push(x);
-}
-let Greece = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[9].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Greece.push(x);
-}
-let Spain = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[10].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Spain.push(x);
-}
-let France = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[11].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  France.push(x);
-}
-let Croatia = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[12].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Croatia.push(x);
-}
-let Italy = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[13].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Italy.push(x);
-}
-let Cyprus = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[14].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Cyprus.push(x);
-}
-let Latvia = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[15].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Latvia.push(x);
-}
-let Lithuania = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[16].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Lithuania.push(x);
-}
-let Luxembourg = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[17].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Luxembourg.push(x);
-}
-let Hungary = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[18].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Hungary.push(x);
-}
-let Malta = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[19].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Malta.push(x);
-}
-let Netherlands = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[20].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Netherlands.push(x);
-}
-let Austria = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[21].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Austria.push(x);
-}
-let Poland = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[22].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Poland.push(x);
-}
-let Portugal = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[23].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Portugal.push(x);
-}
-let Romania = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[24].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Romania.push(x);
-}
-let Slovenia = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[25].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Slovenia.push(x);
-}
-let Slovakia = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[26].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Slovakia.push(x);
-}
-let Finland = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[27].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Finland.push(x);
-}
-let Sweden = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[28].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Sweden.push(x);
-}
-let Iceland = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[29].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Iceland.push(x);
-}
-let Liechtenstein = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[30].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Liechtenstein.push(x);
-}
-let Norway = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[31].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Norway.push(x);
-}
-let Switzerland = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[32].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Switzerland.push(x);
-}
-let Montenegro = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[33].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Montenegro.push(x);
-}
-let Macedonia = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[34].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Montenegro.push(x);
-}
-let Serbia = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[35].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Serbia.push(x);
-}
-let Turkey = [];
-for (var i = 2; i < table1.rows[2].cells.length; i++) {
-  let x = table1.rows[36].cells[i].innerHTML;
-  x = x.replace(",", ".");
-  x = Number(x);
-  Turkey.push(x);
-}
-
-let crimeRate = [
-  Belgium,
-  Bulgaria,
-  Czech,
-  Denmark,
-  Germany,
-  Estonia,
-  Ireland,
-  Greece,
-  Spain,
-  France,
-  Croatia,
-  Italy,
-  Cyprus,
-  Latvia,
-  Lithuania,
-  Luxembourg,
-  Hungary,
-  Malta,
-  Netherlands,
-  Austria,
-  Poland,
-  Portugal,
-  Romania,
-  Slovenia,
-  Slovakia,
-  Finland,
-  Sweden,
-  Iceland,
-  Liechtenstein,
-  Norway,
-  Switzerland,
-  Montenegro,
-  Macedonia,
-  Serbia,
-  Turkey,
-];
 let colours = [
   "#d71dbc",
   "#630d56",
@@ -360,7 +75,7 @@ let allData = [];
 for (i = 0; i < 35; i++) {
   var object = {
     label: countries[i],
-    data: crimeRate[i],
+    data: data1[i],
     borderColor: colours[i],
     fill: false,
   };
@@ -373,21 +88,6 @@ const data = {
 };
 
 const canvas = document.createElement("canvas");
-new Chart(canvas, {
-  type: "line",
-  data: data,
-  options: {
-    animation: false,
-    aspectRatio: 1,
-    responsive: true,
-    scales: {
-      y: {
-        type: "logarithmic",
-      },
-    },
-  },
-});
-table1.parentNode.insertBefore(canvas, table1);
 
 //2eme graph
 
@@ -398,23 +98,26 @@ const year2 = table2.rows[0].cells[3].innerHTML;
 
 function createArrayObject(length, array) {
   for (let i = 0; i < length; i++) {
-    array.push({ label: [], data: [] });
+    array.push({ label: [], data: [], backgroundColor: colours[i] });
   }
   return array;
 }
-//les pays
 let table2Object = [];
+
 createArrayObject(table2.rows.length - 1, table2Object);
 
 for (let i = 1; i < table2.rows.length; i++) {
-  table2Object[i - 1].label.push(table2.rows[i].cells[1].innerHTML);
+  if (table2.rows[i].cells[1].innerHTML.includes("<br>")) {
+    table2Object[i - 1].label.push(
+      table2.rows[i].cells[1].innerHTML.replace("<br>", " ")
+    );
+  } else table2Object[i - 1].label.push(table2.rows[i].cells[1].innerHTML);
 }
 
-//le premier élement c'est l'année puis le taux de crime
 for (let i = 1; i < table2.rows.length; i++) {
   table2Object[i - 1].data.push(table2.rows[i].cells[2].innerHTML);
 }
-//le premier élement c'est l'année puis le taux de crime année 2
+
 for (let i = 1; i < table2.rows.length; i++) {
   table2Object[i - 1].data.push(table2.rows[i].cells[3].innerHTML);
 }
@@ -425,11 +128,6 @@ const data2 = {
   datasets: table2Object,
 };
 
-new Chart(canvas2, {
-  type: "bar",
-  data: data2,
-});
-table2.parentNode.insertBefore(canvas2, table2);
 //fetch
 
 const firstHeading = document.getElementById("firstHeading");
@@ -438,6 +136,29 @@ canvas3.setAttribute("id", "chartContainer");
 firstHeading.parentNode.insertBefore(canvas3, firstHeading);
 
 window.onload = function () {
+  //GRAPH 1
+  new Chart(canvas, {
+    type: "line",
+    data: data,
+    options: {
+      animation: false,
+      aspectRatio: 1,
+      responsive: true,
+      scales: {
+        y: {
+          type: "logarithmic",
+        },
+      },
+    },
+  });
+  table1.parentNode.insertBefore(canvas, table1);
+  //GRAPH 2
+  new Chart(canvas2, {
+    type: "bar",
+    data: data2,
+  });
+  table2.parentNode.insertBefore(canvas2, table2);
+  //GRAPH 3
   var dataPoints = [];
   var chart;
   chart = new Chart(canvas3, {
